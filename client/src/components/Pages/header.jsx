a26109748fbc66403ff75c8b065c676a6a0e58f8
harshit <harshitkumar9548@gmail.com>
2024-05-20 08:48:56 +0530

    Final commit

---
+++ client/src/components/Pages/header.jsx
@@ -0,0 +1,51 @@
+import NavBar from "./navbar";
+import { Link } from "react-router-dom";
+import auth from "../utils/auth";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import Background from "../images/jet-airways.webp";
+function Header() {
+  const [user, setUser] = useState({ user_id: null, first_name: "", last_name: "Guest" });
+  const [admin, setAdmin] = useState(false);
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    const loggedInAdmin = localStorage.getItem("admin");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser[0]);
+    } else if (loggedInAdmin) {
+      const foundAdmin = JSON.parse(loggedInAdmin);
+      setAdmin(foundAdmin[0]);
+    }
+  }, []);
+
+  return (
+    <header>
+      <div
+        className="p-5 text-center bg-image"
+        style={{
+          backgroundImage: `url(${Background})`,
+          height: "400px",
+        }}
+      >
+        <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="text-white">
+              <h1 className="mb-3">Welcome to SkyLink Airways</h1>
+              {!admin && <h4 className="mb-3">{user.first_name + " " + user.last_name} </h4>}
+              {admin && <h4 className="mb-3">Admin {admin.name}</h4>}
+              <button className="btn btn-outline-light btn-lg">
+                <Link style={{ color: "white" }} to={"/serachFlights/" + user.user_id}>
+                  BOOK
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ server/dbconfig.js
@@ -0,0 +1,33 @@
+import mysql from "mysql2";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//create connection
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  // ssl: {
+  //   ca: fs.readFileSync(__dirname + "/DigiCertGlobalRootCA.crt.pem"),
+  // },
+});
+
+//connect
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("database connected successfully!!!!!");
+  }
+});
+
+export default db;
